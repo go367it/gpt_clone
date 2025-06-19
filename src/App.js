4c6f2afd541a174a417af07b26dc1ea1a6ab2503
go367it <satyamkumar3001@gmail.com>
2025-06-20 00:50:32 +0530

    added ant ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Flex } from 'antd';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <Flex align='center' gap="middle" justify='center'>
+        <Button type='primary'>Primary</Button>
+        <Button type='primary'>Primary</Button>
+      </Flex>
+    
   );
 }
 
